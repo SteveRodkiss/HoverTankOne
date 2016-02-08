a815796f3a84358ac0913151188ce419b72943bf
SteveRodkiss <srodkiss@rosmini.school.nz>
2016-02-08 16:19:03 +1300

    Good Start to flight Component
    Basic Scene Set up and playable

---
+++ Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camerafollow : MonoBehaviour
+{
+
+    public GameObject Target;
+    public Vector3 Offset = new Vector3(0, 5, -20);
+    	
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 TransformedOffset = Target.transform.TransformPoint(Offset);
+        transform.position = TransformedOffset;
+        transform.LookAt(Target.transform);
+    }
+}
---
+++ Assets/Scripts/SpaceShipControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Spaceship flight controls
+public class SpaceShipControl : MonoBehaviour
+{
+    float Thrust = 5000f;
+    float TurnForce = 1000f;
+    float PitchForce = 5000f;
+    Rigidbody rb;
+
+    //these values use to calculate pitch and bank of the spaceship mesh
+    public float maxturnrate = 2f;
+    public float maxclimbrate = 20f;
+
+    //the mesh gets rotated to simulate banking
+    public Transform mesh;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+ 
+
+    void FixedUpdate()
+    {
+        //input for the pitch
+        float pitch = Input.GetAxis("Vertical");
+        rb.AddRelativeForce(new Vector3(0, pitch * PitchForce, 0));
+
+        //now the turn-- y rotation only! x and z are frozen in the rigidbody constraints
+        float turn = Input.GetAxis("Horizontal");
+        rb.AddTorque(new Vector3(0, turn * TurnForce, 0));
+
+        //thrust
+        rb.AddRelativeForce(new Vector3(0, 0, Thrust));
+
+        //now pitch and roll of mesh
+        float turnvalue = (rb.angularVelocity.y / maxturnrate) * -90f;
+        float pitchvalue = (rb.velocity.y / maxclimbrate) * -30f;
+        //rotate the lower level mesh to simulate the banking and pitching
+        Quaternion rotation = Quaternion.Euler(pitchvalue, 0, turnvalue);
+        mesh.localRotation = rotation;
+    }
+
+
+
+
+
+}
