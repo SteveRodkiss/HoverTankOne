4561c05f8896db586d40c1ff7c11929b08defa46
SteveRodkiss <srodkiss@rosmini.school.nz>
2016-02-08 17:37:22 +1300

    Lasers and Particle Systems added

---
+++ Assets/Scripts/DestroyAfterSeconds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+    public float Seconds = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, Seconds);
+    }
+}
---
+++ Assets/Scripts/LaserGun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserGun : MonoBehaviour
+{
+
+    public GameObject Laser;
+    //fire rate of bullet
+    public float FireRate = 10f;
+    float Delay = 1f;
+    float Timer = 0f;
+
+    void Start()
+    {
+        Delay = 1f / FireRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Input.GetButton("Fire1") && Timer >= Delay)
+        {
+            Timer = 0f;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(Laser, transform.position, transform.rotation);
+    }
+}
